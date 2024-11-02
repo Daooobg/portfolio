0492168c73ff20296c5307e404cdb9cae906ad92
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-02 15:31:47 +0000

    Add new color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
                 background: 'var(--background)',
                 foreground: 'var(--foreground)',
                 light: { primary: '#F5F5F5' },
-                dark: { black: '#000000', gray: '#616161', darkGray: '#202020' },
+                dark: { black: '#000000', gray: '#616161', darkGray: '#202020', lightGray: '#9C9C9C' },
                 green: { primary: '#2BCF7E', secondary: '#ACFFD6' },
             },
         },
