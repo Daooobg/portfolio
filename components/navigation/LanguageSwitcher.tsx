5e77c86c7cfe66c50638dc3c7639c98caa6c5fc6
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-02 10:10:30 +0000

    Create language switcher

--- components/navigation/LanguageSwitcher.tsx
+++ components/navigation/LanguageSwitcher.tsx
@@ -1,12 +1,28 @@
 'use client';
 
-import setLanguage from "@/actions/setLanguages";
+import { useLocale } from 'next-intl';
+
+import setLanguage from '@/actions/setLanguages';
 
 export default function LanguageSwitcher() {
+    const locale = useLocale();
+
     return (
-        <select onChange={(e) => setLanguage(e.target.value)}>
-            <option value="en">En</option>
-            <option value="bg">Bg</option>
-        </select>
+        <div className="flex gap-1.5 items-center">
+            <button
+                className={`${locale === 'en' ? 'unActiveLanguageBtn' : 'activeLanguageBtn'} `}
+                disabled={locale === 'en'}
+                onClick={() => setLanguage('en')}
+            >
+                EN
+            </button>
+            <button
+                className={`${locale === 'bg' ? 'unActiveLanguageBtn' : 'activeLanguageBtn'} `}
+                disabled={locale === 'bg'}
+                onClick={() => setLanguage('bg')}
+            >
+                BG
+            </button>
+        </div>
     );
 }
