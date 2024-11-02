3b5a50e7dbb0754139b48399844c8b5cf82c3c6d
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-02 15:32:16 +0000

    Add image remote pattern

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [{ hostname: 'res.cloudinary.com' }],
+    },
+};
 
 module.exports = withNextIntl(nextConfig);
