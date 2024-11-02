3a0fc782507c1e3a8529af105d5bf3cb2fda1bfd
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-02 10:08:27 +0000

    Add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
+                light: { primary: '#F5F5F5' },
+                dark: { black: '#000000', gray: '#616161', darkGray: '#202020' },
+                green: { primary: '#2BCF7E', secondary: '#ACFFD6' },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
