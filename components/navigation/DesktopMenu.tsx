61907dda9de31ce8d60b9bdd16d9d85916b232e9
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-03 12:24:58 +0000

    clean imports

--- components/navigation/DesktopMenu.tsx
+++ components/navigation/DesktopMenu.tsx
@@ -4,9 +4,6 @@ import Link from 'next/link';
 import ArrowBottom from '@/ui/design/ArrowBottom';
 import ArrowRight from '@/ui/design/ArrowRight';
 import LanguageSwitcher from './LanguageSwitcher';
-import ArrowRightSvg from '/public/svgs/arrow_right_alt.svg';
-import Image from 'next/image';
-
 
 export default function DesktopMenu() {
     const t = useTranslations('Navigation');
@@ -26,13 +23,13 @@ export default function DesktopMenu() {
                 </li>
                 <li>
                     <button className="tertiaryBtn flex items-center gap-1 group">
-                        <ArrowBottom className='fill-light-primary group-hover:fill-green-primary group-hover:transform group-hover:duration-200 group-hover:transition group-hover:ease-out' />
+                        <ArrowBottom className="fill-light-primary group-hover:fill-green-primary group-hover:transform group-hover:duration-200 group-hover:transition group-hover:ease-out" />
                         {t('DownloadCV')}
                     </button>
                 </li>
                 <li>
                     <Link href="#" className="secondaryBtn group">
-                        <ArrowRight className='fill-green-primary group-hover:fill-dark-black group-hover:transform group-hover:duration-200 group-hover:transition group-hover:ease-out'  />
+                        <ArrowRight className="fill-green-primary group-hover:fill-dark-black group-hover:transform group-hover:duration-200 group-hover:transition group-hover:ease-out" />
                         <p>{t('ContactMe')}</p>
                     </Link>
                 </li>
