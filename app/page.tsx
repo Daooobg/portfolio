b9be03bd94b7db6d57090539cccaac8e0d4252bb
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-03 09:09:01 +0000

    Add skills section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from '@components/heroSection/HeroSection';
+import SkillsSection from '@components/skillsSection/SkillsSection';
 
 export default function Home() {
     return (
         <>
             <main>
                 <HeroSection />
+                <SkillsSection />
             </main>
             <footer></footer>
         </>
