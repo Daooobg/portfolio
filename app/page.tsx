1562f8393a90ffbd26abf889deeb71be08abc193
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-05 14:02:40 +0000

    Add contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import sendEmail from '@actions/sendEmail';
+import ContactSection from '@components/contactSection/ContactSection';
 import HeroSection from '@components/heroSection/HeroSection';
 import SkillsSection from '@components/skillsSection/SkillsSection';
 
 export default function Home() {
     return (
         <>
-            <main className='max-w-[1240px] mx-auto'>
+            <main className="max-w-[1240px] mx-auto">
                 <HeroSection />
                 <SkillsSection />
+                <ContactSection action={sendEmail} />
             </main>
         </>
     );
