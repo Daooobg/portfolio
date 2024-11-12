964b6d69cf9a2546b3e5cbf64ea60026e3099084
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-12 09:32:16 +0000

    Add DashboardLayout for auth group route

---
+++ app/(auth)/dashboard/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next';
+
+import '../../globals.css';
+import Footer from '@components/footer/Footer';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+    title: 'Ivaylo Ivanov Portfolio',
+    description: 'Dashboard',
+};
+
+export default async function DashboardLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="bg-dark-black">
+            <nav className="flex justify-end gap-6 p-4 items-center md:px-20 md:py-10 max-w-[1240px] mx-auto">
+                <Link href="/dashboard/addPortfolio" className="secondaryBtn">
+                    Add Portfolio
+                </Link>
+                <Link href="/dashboard/settings" className="secondaryBtn">
+                    Settings
+                </Link>
+                <form>
+                    <button type="submit" className="secondaryBtn">
+                        Logout
+                    </button>
+                </form>
+            </nav>
+            <main className="max-w-[1240px] mx-auto">{children}</main>
+            <Footer />
+        </div>
+    );
+}
