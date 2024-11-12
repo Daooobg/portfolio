4b4b8034e545c40b57dd08a2d335ca39769da467
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-12 06:38:04 +0000

    Implement user registration

---
+++ actions/authAction.ts
@@ -0,0 +1,45 @@
+'use server';
+
+import { hashUserPassword } from '@/lib/hash';
+import { createAuthSession } from '@/lib/luciaSessions';
+import { createUser } from '@/lib/user';
+import { redirect } from 'next/navigation';
+
+export async function register(formState: unknown, formData: FormData) {
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    const errors = [];
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email || email.trim().length === 0) {
+        errors.push('Please enter your email address.');
+    } else if (!emailRegex.test(email)) {
+        errors.push('Please enter a valid email address.');
+    }
+
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!password || password.trim().length === 0) {
+        errors.push('Please enter your name.');
+    } else if (!passwordRegex.test(password)) {
+        errors.push(
+            'Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character (e.g., @$!%*?&).'
+        );
+    }
+
+    if (errors.length > 0) {
+        return { errors: errors };
+    }
+
+    const hashedPassword = hashUserPassword(password);
+    try {
+        const user = await createUser(email, hashedPassword);
+        await createAuthSession(user.id);
+    } catch (error) {
+        if (error instanceof Error) {
+            return { errors: [error.message] };
+        }
+        return { errors: ['Something went wrong'] };
+    }
+    redirect('/dashboard');
+}
