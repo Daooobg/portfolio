b970dfa2b3d08a40854e12f1af77cd722c9c9109
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-12 06:02:24 +0000

    Add AuthForm

---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from '@components/authForm/AuthForm';
+
+export default function Auth() {
+    return (
+        <main className="max-w-[1240px] mx-auto">
+            <AuthForm />
+        </main>
+    );
+}
