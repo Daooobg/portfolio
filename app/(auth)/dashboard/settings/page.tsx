155a662e9d81bb7b29be6cc70b632cd676a4225a
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-13 17:47:45 +0000

    Integrate getSettings in Settings page and pass data to SettingsSection

--- app/(auth)/dashboard/settings/page.tsx
+++ app/(auth)/dashboard/settings/page.tsx
@@ -1,5 +1,8 @@
+import { getSettings } from '@actions/settingsActions';
 import SettingsSection from '@components/dashboard/settings/SettingsSection';
 
-export default function Settings() {
-    return <SettingsSection />;
+export default async function Settings() {
+    const data = await getSettings();
+
+    return <SettingsSection settingsData={data} />;
 }
