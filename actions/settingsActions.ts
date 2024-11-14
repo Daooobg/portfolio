b5fe05beacf0d6a2b35ee75da998ea7a7753ebe3
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-14 16:14:32 +0000

    Add updateSettings action to handle form submissions and update settings

--- actions/settingsActions.ts
+++ actions/settingsActions.ts
@@ -1,7 +1,27 @@
 'use server';
 
-import { getSettingsById } from '@/lib/settings';
+import { getSettingsById, updateSettingsById } from '@/lib/settings';
 
 export async function getSettings() {
     return await getSettingsById(1);
+}
+
+export async function updateSettings(formState: unknown, formData: FormData) {
+    const form = Object.fromEntries(formData.entries());
+
+    const activeRegister = form['activeRegister'] === 'on' ? true : false; // Checkbox value
+    const visibility = parseInt(form['visibility'] as string); // Parse visibility
+
+    try {
+        await updateSettingsById(2, activeRegister, visibility);
+    } catch (error) {
+        console.log(error);
+        return { errors: ['Problem'] };
+    }
+
+    return {
+        activeRegister,
+        visibility,
+        errors: [],
+    };
 }
