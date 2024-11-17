f52c310cb7dbdc61004d8e7111df87f4422d798a
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-11-17 16:35:54 +0000

    Add PortfolioSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import sendEmail from '@actions/sendEmail';
 import ContactSection from '@components/contactSection/ContactSection';
 import HeroSection from '@components/heroSection/HeroSection';
+import PortfolioSection from '@components/portfolioSection/PortfolioSection';
 import SkillsSection from '@components/skillsSection/SkillsSection';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
         <>
             <main className="max-w-[1240px] mx-auto">
                 <HeroSection />
+                <PortfolioSection />
                 <SkillsSection />
                 <ContactSection action={sendEmail} />
             </main>
